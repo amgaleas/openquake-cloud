139538aec529dfd8ff74c9772876a994e89d088d
amgaleas <130239311+amgaleas@users.noreply.github.com>
2024-06-05 21:57:04 -0500

    Update ejecutar.sh

--- ejecutar.sh
+++ ejecutar.sh
@@ -23,5 +23,36 @@ fi
 # Reemplazar el valor de region_grid_spacing en el archivo .ini
 sed -i "s/region_grid_spacing = .*/region_grid_spacing = $nuevo_grid_spacing/" "$archivo_ini"
 
+# Inicia monitoreo de CPU y RAM
+echo "Iniciando monitoreo de recursos..."
+vmstat 1 > vmstat.log &
+VMSTAT_PID=$!
+
+# Captura el tiempo de inicio
+start_time=$(date +%s)
+
 # Ejecutar el comando OpenQuake
 oq engine --run "$archivo_ini"
+
+# Detener el monitoreo de recursos
+kill $VMSTAT_PID
+
+# Captura el tiempo de finalización
+end_time=$(date +%s)
+
+# Calcula el tiempo total de ejecución
+execution_time=$((end_time - start_time))
+
+# Encontrar el último archivo HDF5 generado
+latest_hdf5=$(ls -t /root/oqdata/calc_*.hdf5 | head -n 1)
+size_kb=$(du -k "$latest_hdf5" | cut -f1)
+
+# Cálculos de uso promedio de CPU y RAM
+cpu_usage=$(awk '{total += $13} END {print total/NR}' vmstat.log)
+ram_usage=$(awk '{total += $4} END {print total/NR/1024}' vmstat.log) # Convertido a GB
+
+# Mostrar resultados
+echo "Uso promedio de CPU: $cpu_usage %"
+echo "Uso promedio de RAM: $ram_usage GB"
+echo "Tamaño del archivo: $size_kb KB"
+echo "Tiempo de ejecución: $execution_time segundos"
